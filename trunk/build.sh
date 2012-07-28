ce0b3fd3f1f052e6442288f5bfd34006b3dc1815
ambrop7 <ambrop7@25b46dc6-fb25-11de-84a7-55c42b2a066e>
2012-07-28 12:53:47 +0000

    update README, add -m32 to build script

--- trunk/build.sh
+++ trunk/build.sh
@@ -8,10 +8,10 @@ if [ -z "$DEST" ]; then
 	exit 1
 fi
 
-"$WINEGCC" -o "$DEST/cl" wrapmsvc.c -DWRAP_CL -lmsvcrt &&
-"$WINEGCC" -o "$DEST/link" wrapmsvc.c -DWRAP_LINK -lmsvcrt &&
-"$WINEGCC" -o "$DEST/rc" wrapmsvc.c -DWRAP_RC -lmsvcrt &&
-"$WINEGCC" -o "$DEST/mt" wrapmsvc.c -DWRAP_MT -lmsvcrt &&
+"$WINEGCC" -m32 $CFLAGS -o "$DEST/cl" wrapmsvc.c -DWRAP_CL -lmsvcrt &&
+"$WINEGCC" -m32 $CFLAGS -o "$DEST/link" wrapmsvc.c -DWRAP_LINK -lmsvcrt &&
+"$WINEGCC" -m32 $CFLAGS -o "$DEST/rc" wrapmsvc.c -DWRAP_RC -lmsvcrt &&
+"$WINEGCC" -m32 $CFLAGS -o "$DEST/mt" wrapmsvc.c -DWRAP_MT -lmsvcrt &&
 ln -sf cl.exe "$DEST/cl" &&
 ln -sf link.exe "$DEST/link" &&
 ln -sf rc.exe "$DEST/rc" &&
